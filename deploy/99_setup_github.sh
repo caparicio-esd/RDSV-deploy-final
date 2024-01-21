440cf4aac382d87625b0e98578b8e9b7e225c61b
caparicio-esd <caparicio@esdmadrid.es>
2024-01-21 16:07:00 +0100

    change github script

--- deploy/99_setup_github.sh
+++ deploy/99_setup_github.sh
@@ -2,4 +2,6 @@ ssh-keygen -t ed25519 -C "caparicio@esdmadrid.es"
 cat /home/upm/.ssh/id_ed25519.pub
 git init
 git remote add origin https://github.com/caparicio-esd/RDSV-deploy-final
-git config
+git config --global user.name "caparicio-esd"
+git config --global user.mail "caparicio@esdmadrid.es"
+git config --global user.email "caparicio@esdmadrid.es"
